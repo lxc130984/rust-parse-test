d49dca6fd716585bfe751526092ad0602fceee87
lxcc <3777859884@qq.com>
2026-05-06 00:46:33 +0800

    comparsion and basic logic

--- src/ast.rs
+++ src/ast.rs
@@ -2,6 +2,46 @@ use chumsky::{error::RichReason, prelude::*, util::Maybe};
 use ariadne::{ColorGenerator,Source,Label,Report};
 use crate::eval::Expr;
 
+// fn logical_parse<'src>()->impl Parser<'src, &'src str ,Expr<'src>,extra::Err<Rich<'src,char>>>{
+//     comparsion_parse()
+//         .or(bool_parser())
+// 	.foldl(
+// 	    choice((
+// 		just("&&").padded().to(Expr::And as fn(_,_)->_),
+// 		just("||").padded().to(Expr::Or as fn(_,_)->_),
+// 	    ))
+// 		.then(comparsion_parse().or(bool_parser()))
+// 		.repeated()
+// 		,
+// 	    |l,(op,r)|{op(Box::new(l),Box::new(r))}
+// 	)
+	
+// }
+
+
+
+// fn comparsion_parse<'src>()->impl Parser<'src, &'src str ,Expr<'src>,extra::Err<Rich<'src,char>>>{
+//     arithmetic_parse()
+//         .or(bool_parser())
+//         .then(choice((
+// 	    just("==").padded().to(Expr::Eq as fn(_, _) -> _),
+//             just("!=").padded().to(Expr::Neq as fn(_, _) -> _),
+//             just(">=").padded().to(Expr::Ge as fn(_, _) -> _),
+//             just("<=").padded().to(Expr::Le as fn(_, _) -> _),
+//             just(">").padded().to(Expr::Gt as fn(_, _) -> _),
+//             just("<").padded().to(Expr::Lt as fn(_, _) -> _),
+// 	))
+// 	      .then(arithmetic_parse().or(bool_parser()))
+// 	      .or_not()
+// 	)
+//         .map(|(l,o_r)|{
+// 	    match o_r{
+// 		Some((op,r))=>{op(Box::new(l),Box::new(r))},
+// 		None=>{l},
+// 	    }
+// 	})
+        
+// }
 
 fn arithmetic_parse<'src>()->impl Parser<'src, &'src str ,Expr<'src>,extra::Err<Rich<'src,char>>>{
     let int = text::int::<_,extra::Err<Rich<_>>>(10).padded().map(|s:&str|{
@@ -38,20 +78,66 @@ pub fn bool_parser<'src>()->impl Parser<'src,&'src str,Expr<'src>,extra::Err<Ric
         .or(just("true").padded().to(Expr::Bool(true)))
 }
 
-pub fn parser<'src>()->impl Parser<'src,&'src str,Expr<'src>,extra::Err<Rich<'src,char>>>{
-    recursive(|expr|{
-	
-	let block = expr
-	    .separated_by(just(';').padded())
+fn comparsion_parser<'src>(
+    expr:impl Parser<'src,&'src str,Expr<'src>,extra::Err<Rich<'src,char>>>+Clone+'src
+)->impl Parser<'src,&'src str,Expr<'src>,extra::Err<Rich<'src,char>>>{
+    let comparsion_expected=arithmetic_parse()
+            .or(bool_parser())
+        .or(block_parser(expr.clone()))
+        .boxed();
+    comparsion_expected.clone()
+            .then(choice((
+		just("==").padded().to(Expr::Eq as fn(_, _) -> _),
+		just("!=").padded().to(Expr::Neq as fn(_, _) -> _),
+		just(">=").padded().to(Expr::Ge as fn(_, _) -> _),
+		just("<=").padded().to(Expr::Le as fn(_, _) -> _),
+		just(">").padded().to(Expr::Gt as fn(_, _) -> _),
+		just("<").padded().to(Expr::Lt as fn(_, _) -> _),
+	    ))
+		  .then(comparsion_expected)
+		  .or_not()
+	    )
+            .map(|(l,o_r)|{
+		match o_r{
+		    Some((op,r))=>{op(Box::new(l),Box::new(r))},
+		    None=>{l},
+		}
+	    })
+}
+
+fn logic_parser<'src>(
+    comparsion:impl Parser<'src,&'src str,Expr<'src>,extra::Err<Rich<'src,char>>>+Clone+'src
+)->impl Parser<'src,&'src str,Expr<'src>,extra::Err<Rich<'src,char>>>{
+    let comparsion_and_bool=comparsion.or(bool_parser()).boxed();
+    comparsion_and_bool.clone()
+	    .foldl(
+		choice((
+		    just("&&").padded().to(Expr::And as fn(_,_)->_),
+		    just("||").padded().to(Expr::Or as fn(_,_)->_),
+		))
+		    .then(comparsion_and_bool)
+		    .repeated()
+		    ,
+		|l,(op,r)|{op(Box::new(l),Box::new(r))}
+	    )
+}
+
+
+fn block_parser<'src>(
+    expr:impl Parser<'src,&'src str,Expr<'src>,extra::Err<Rich<'src,char>>>
+)->impl Parser<'src,&'src str,Expr<'src>,extra::Err<Rich<'src,char>>>{
+    expr.separated_by(just(';').padded())
 	    .collect::<Vec<_>>()
 	    .delimited_by(just('{').padded(),just('}').padded()).padded()
-	    .map(Expr::Block);
+	    .map(Expr::Block)
+}
 
-	arithmetic_parse()
-	    .or(bool_parser())
-	    .or(block)
-	    .boxed()
-	
+pub fn parser<'src>()->impl Parser<'src,&'src str,Expr<'src>,extra::Err<Rich<'src,char>>>{
+    recursive(|expr|{
+	let comparsion = comparsion_parser(expr.clone()).boxed();
+	let logic = logic_parser(comparsion).boxed();
+	let block = block_parser(expr).boxed();
+	logic.or(block.clone())
     })
 }
 
--- src/eval.rs
+++ src/eval.rs
@@ -37,6 +37,58 @@ impl Value{
 	    _=>{Err("error when eval_add".to_string())}
 	}
     }
+
+    fn value_eq(a_value:Value,b_value:Value)->Result<Value,String>{
+	match (a_value,b_value){
+	    (Value::Num(a),Value::Num(b))=>{Ok(Value::Bool(a==b))},
+	    (Value::Bool(a),Value::Bool(b))=>{Ok(Value::Bool(a==b))},
+	    _=>{Err("error when eval_eq".to_string())}
+	}
+    }
+    fn value_neq(a_value:Value,b_value:Value)->Result<Value,String>{
+	match (a_value,b_value){
+	    (Value::Num(a),Value::Num(b))=>{Ok(Value::Bool(a!=b))},
+	    (Value::Bool(a),Value::Bool(b))=>{Ok(Value::Bool(a==b))},
+	    _=>{Err("error when eval_neq".to_string())}
+	}
+    }
+    fn value_gt(a_value:Value,b_value:Value)->Result<Value,String>{
+	match (a_value,b_value){
+	    (Value::Num(a),Value::Num(b))=>{Ok(Value::Bool(a>b))},
+	    _=>{Err("error when eval_gt".to_string())}
+	}
+    }
+    fn value_lt(a_value:Value,b_value:Value)->Result<Value,String>{
+	match (a_value,b_value){
+	    (Value::Num(a),Value::Num(b))=>{Ok(Value::Bool(a<b))},
+	    _=>{Err("error when eval_lt".to_string())}
+	}
+    }
+    fn value_ge(a_value:Value,b_value:Value)->Result<Value,String>{
+	match (a_value,b_value){
+	    (Value::Num(a),Value::Num(b))=>{Ok(Value::Bool(a>=b))},
+	    _=>{Err("error when eval_ge".to_string())}
+	}
+    }
+    fn value_le(a_value:Value,b_value:Value)->Result<Value,String>{
+	match (a_value,b_value){
+	    (Value::Num(a),Value::Num(b))=>{Ok(Value::Bool(a<=b))},
+	    _=>{Err("error when eval_le".to_string())}
+	}
+    }
+
+    fn value_and(a_value:Value,b_value:Value)->Result<Value,String>{
+	match (a_value,b_value){
+	    (Value::Bool(a),Value::Bool(b))=>{Ok(Value::Bool(a&&b))},
+	    _=>{Err("error when eval_and".to_string())}
+	}
+    }
+    fn value_or(a_value:Value,b_value:Value)->Result<Value,String>{
+	match (a_value,b_value){
+	    (Value::Bool(a),Value::Bool(b))=>{Ok(Value::Bool(a||b))},
+	    _=>{Err("error when eval_or".to_string())}
+	}
+    }
 }
 
 
@@ -53,6 +105,16 @@ pub enum Expr<'src>{
     Mul(Box<Expr<'src>>,Box<Expr<'src>>),
     Div(Box<Expr<'src>>,Box<Expr<'src>>),
 
+    Eq(Box<Expr<'src>>,Box<Expr<'src>>),
+    Neq(Box<Expr<'src>>,Box<Expr<'src>>),
+    Gt(Box<Expr<'src>>,Box<Expr<'src>>), //>
+    Lt(Box<Expr<'src>>,Box<Expr<'src>>), //<
+    Ge(Box<Expr<'src>>,Box<Expr<'src>>), //>=
+    Le(Box<Expr<'src>>,Box<Expr<'src>>), //<=
+
+    And(Box<Expr<'src>>,Box<Expr<'src>>),
+    Or(Box<Expr<'src>>,Box<Expr<'src>>),
+    
     Block(Vec<Expr<'src>>),
     
 }
@@ -80,13 +142,38 @@ pub fn eval<'src>(
 	Expr::Bool(b)=>{
 	    Ok(Value::Bool(*b))
 	},
+	Expr::Eq(a,b)=>{
+	    Value::value_eq(eval(a)?,eval(b)?)
+	},
+	Expr::Neq(a,b)=>{
+	    Value::value_neq(eval(a)?,eval(b)?)
+	},
+	Expr::Gt(a,b)=>{
+	    Value::value_gt(eval(a)?,eval(b)?)
+	},
+	Expr::Lt(a,b)=>{
+	    Value::value_lt(eval(a)?,eval(b)?)
+	},
+	Expr::Ge(a,b)=>{
+	    Value::value_ge(eval(a)?,eval(b)?)
+	},
+	Expr::Le(a,b)=>{
+	    Value::value_le(eval(a)?,eval(b)?)
+	},
+	Expr::And(a,b)=>{
+	    Value::value_and(eval(a)?,eval(b)?)
+	},
+	Expr::Or(a,b)=>{
+	    Value::value_or(eval(a)?,eval(b)?)
+	}
 	Expr::Block(vec)=>{
 	    let mut last = Value::Unit;
 	    for e in vec{
 		last = eval(e)?;
 	    }
 	    Ok(last)
-	}
+	},
+	
 	
 	
 	_=>{todo!()}
